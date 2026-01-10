bbd47612b65597a28804264e027bb879eefe57ff
zxkuhl <zero98122@gmail.com>
2026-01-10 00:26:28 -0600

    Update loader.lua

--- scr/loader.lua
+++ scr/loader.lua
@@ -1,6 +1,6 @@
 -- config
-local repoName = "executor-gui"
-local repoOwner = "jLn0n"
+local repoName = "Idek-rn"
+local repoOwner = "zxkuhl"
 -- variables
 local http_request = (syn and syn.request) or (http and http.request) or request or http_request
 local wrapperEnv = {}
